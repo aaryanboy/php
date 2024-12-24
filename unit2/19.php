a9cae4dd3ae661a05dceb7b9b886ef267b36b463
aaryanboy <aaryanboy12@gmail.com>
2024-12-24 18:37:13 +0545

    rechange of commen erroir

--- unit2/19.php
+++ unit2/19.php
@@ -8,7 +8,7 @@ echo $currentDate->format('F j, Y, g:i A') . "\n";
 echo $currentDate->format('Ymd') . "\n";
 echo $currentDate->format('H:i:s, d-m-y') . "\n"; 
 echo "It is the " . $currentDate->format('j') . "th day.\n";  
-echo $currentDate->format('D. M j Y g:i:s T') . "\n";  // Fri. Jul 12 2024 8:27:46 CEST
-echo $currentDate->format('H:i:s') . "\n";  // 08:27:46
-echo $currentDate->format('Y-m-d H:i:s') . "\n";  // 2024-07-12 08:27:46
+echo $currentDate->format('D. M j Y g:i:s T') . "\n";  
+echo $currentDate->format('H:i:s') . "\n";  
+echo $currentDate->format('Y-m-d H:i:s') . "\n";  
 ?>
