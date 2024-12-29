1f34cea6d47cabebd1e4289fb7342e8d8ee18d25
aaryanboy <aaryanboy12@gmail.com>
2024-12-29 12:45:50 +0545

    error

--- unit 4/5.php
+++ unit 4/5.php
@@ -1,54 +1,13 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Ticket Issuing System</title>
-    <style>
-        .form-box{
-            background-color: white;
-            padding:10px;
-            max-width:450px;
-            margin: 50px auto;
-            border: 3px solid black;
-        }
-        label{
-            font-weight: bold;
-        }
-        fieldset{
-            border:2px solid black;
-        }
-
-    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
 </head>
 <body>
-    <div class="form-box">
-    <form action="process_ticket.php" method="POST" enctype="multipart/form-data" >
-        <fieldset>
-         <legend>Create a Ticket</legend>
-        <label for="username">Username:</label><br>
-        <input type="text" id="username" name="username" required><br><br>
-
-        <label for="email">Email:</label><br>
-        <input type="email" id="email" name="email" required><br><br>
-
-        <label for="phone">Phone:</label><br>
-        <input type="text" id="phone" name="phone" required><br><br>
-
-        <label for="Issue">Issue:</label><br>
-        <select id="Issue" name="Issue" required>
-            <option value="Poor Internet Connection">Poor Internet Connection</option>
-            <option value="Fiber Breakage">Fiber Breakage</option>
-        </select><br><br>
-
-        <label for="comments">Comments:</label><br>
-        <textarea id="comments" name="comments" rows="4" cols="50" required></textarea><br><br>
-
-        <label for="screenshot">Screenshot :</label>
-        <input type="file" id="screenshot" name="screenshot" accept=".png" required><br><br>
-
-        <button type="submit">Submit </button>
-        <button type="reset">Reset</button>
-        </fieldset>
-    </form>
-    </div>
+    <h2>Welcome to the Homepage</h2>
+    <p>You are successfully logged in.</p>
+    
 </body>
 </html>
